aab75258d155a535dd91867daaf8484861b5abfb
closedcaptions13 <closedcaptions13@gmail.com>
2024-05-23 11:43:24 -0400

    added script for enemymovement

--- lunar-looter-unity/Assets/Scripts/EnemyMovementTest.cs
+++ lunar-looter-unity/Assets/Scripts/EnemyMovementTest.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyMovementTest : MonoBehaviour
 {
     public Transform firstPos;
     public Transform secondPos;
-    public Tansform targetPos;
+    public Transform targetPos;
     [SerializeField] private float distance;
     public float speed;
-    void Start()
+
+    void Awake()
     {
         targetPos = firstPos;
     }
+
 
     // Update is called once per frame
     void Update()
@@ -20,14 +23,14 @@ public class EnemyMovementTest : MonoBehaviour
         Move();
     }
 
+
     private void Move(){
         if(Vector2.Distance(transform.position, firstPos.position) < 0.01f){
             targetPos = secondPos;
         }
-        else{
+        if(Vector2.Distance(transform.position, secondPos.position) < 0.01f){
             targetPos = firstPos;
         }
         transform.position = Vector2.MoveTowards(transform.position, targetPos.position, speed*Time.deltaTime);
-
     }
 }
