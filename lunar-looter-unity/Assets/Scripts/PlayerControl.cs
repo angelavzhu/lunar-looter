7cdaeb55e6a69b6a470ad34f0eb5f79222321183
phazrael <angelzhua@gmail.com>
2024-05-27 23:21:27 -0400

    added FOV for player

--- lunar-looter-unity/Assets/Scripts/PlayerControl.cs
+++ lunar-looter-unity/Assets/Scripts/PlayerControl.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    // the movement speed of the player
     public float moveSpeed;
+
+    // the x and y position of the player
     float x,y;
+
+    // the rigidbody of the player
     Rigidbody2D body;
-    public GameObject enemy;
+
+    Vector2 aimDirection;
+
+    // reference to the FOV
+    [SerializeField] private FieldOfView fov;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        // aimDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
@@ -21,13 +30,16 @@ public class PlayerControl : MonoBehaviour
         x = Input.GetAxisRaw("Horizontal") * moveSpeed;
         y = Input.GetAxisRaw("Vertical") * moveSpeed;
         body.velocity = new Vector2(x,y);
+
+        SetAimDirection();
+        fov.SetAim(aimDirection);
+        fov.SetOrigin(transform.position);
     }
 
-// When player hits enemy, kills enemy
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("Enemy")){
-            Destroy(enemy);
-        }
+    private void SetAimDirection(){
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        aimDirection = new Vector2(mouse.x - transform.position.x, 
+                                    mouse.y - transform.position.y);
+
     }
 }
